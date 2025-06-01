e19452e325ddb92273f4524bb267fa3a5a009272
Vaishak <vaishakkrishnamoorthy5@gmail.com>
2025-06-01 23:43:47 +0530

    Fixed Errors - 3

--- src/app/api/order/[id]/route.ts
+++ src/app/api/order/[id]/route.ts
@@ -1,15 +1,19 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> } // note: params is a Promise now
 ) {
-  const { id } = await params;
+  const { id } = await context.params;
+
   try {
     const order = await prisma.order.findUnique({ where: { id } });
-    if (!order)
+
+    if (!order) {
       return NextResponse.json({ error: "Order not found" }, { status: 404 });
+    }
+
     return NextResponse.json(order);
   } catch (error) {
     return NextResponse.json(
