c4cb8389526a6ab73f7f5fa120e18255157de597
Vaishak <vaishakkrishnamoorthy5@gmail.com>
2025-06-01 23:53:43 +0530

    Fixed Errors - 5

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,12 @@
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prisma ?? new PrismaClient();
+const prisma = global.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
